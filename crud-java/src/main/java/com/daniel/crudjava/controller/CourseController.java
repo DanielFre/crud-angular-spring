7feb4bcff3dd5ac741d91e6377a61484a6167d59
Daniel Frey <daniel._.frey@hotmail.com>
2023-03-14 17:58:10 -0300

    Criado metodo post

--- crud-java/src/main/java/com/daniel/crudjava/controller/CourseController.java
+++ crud-java/src/main/java/com/daniel/crudjava/controller/CourseController.java
@@ -2,8 +2,15 @@ package com.daniel.crudjava.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.daniel.crudjava.model.Course;
@@ -19,8 +26,18 @@ public class CourseController {
     private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list() {
+    public @ResponseBody List<Course> list() {
         return courseRepository.findAll();
     }
 
+    // @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course) {
+        // public ResponseEntity<Course> create(@RequestBody Course course) {
+        // System.out.println(course.getName());
+        // return
+        // ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
+        return courseRepository.save(course);
+    }
 }
